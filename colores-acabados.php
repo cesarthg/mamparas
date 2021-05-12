301833455840e6e9f9d91f972474bf5ac80a82ca
César H.G. | @cesarthg <cesar.hg@me.com>
2021-05-12 16:22:29 -0500

    Creacion de los PHP con includ de header y footer de cada uno

--- colores-acabados.php
+++ colores-acabados.php
@@ -5,27 +5,59 @@
   <div class="container">
     <div class="row align-items-center my-5">
       <div class="col-lg-6">
-        <h1 class="font-weight-bold py-2">Modelos y Materiales</h1>
-        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
-          makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
-          you want!</p>
-        <a class="btn btn-primary " href="#!">Ver modelos</a>
-      </div>
-      <div class="col-lg-6">
-        <img class="img-fluid rounded mb-4 mb-lg-0" src="./img/m01.jpg" alt="Modelos y Materiales de Mamparas" /></div>
-    </div>
-    <div class="row align-items-center my-5">
-      <div class="col-lg-6">
-        <img class="img-fluid rounded mb-4 mb-lg-0" src="./img/m02.jpg" alt="Colores y Acabados de Mampras" /></div>
+          <img class="img-fluid rounded mb-4 mb-lg-0" src="./img/colores-acabados-main.jpg" alt="Colores y Acabados de Mampras" /></div>
       <div class="col-lg-6">
         <h1 class="font-weight-bold py-2">Colores y Acabados</h1>
         <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
           makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
           you want!</p>
-        <a class="btn btn-primary py-2" href="#!">Ver acabados</a>
+        <a class="btn btn-primary py-2" href="#!">Ver colores</a>
       </div>
     </div>
   </div>
+  <!-- Modelos -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/mat-01.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 1</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/mat-02.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 2</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/mat-03.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 3</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/mat-04.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 4</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
   <!-- Opciones -->
   <div class="container bg-light">
     <div class="row ml-auto">
@@ -77,48 +109,6 @@
       </div>
     </div>
   </div>
-  <!-- Obras -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 mb-5">
-        <div class="card h-100">
-          <div class="card-body">
-            <img src="./img/mamp-clubfrance.jpg" class="rounded" width="99%" alt="">
-            <h4 class="card-title mt-4">Club France</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at
-              dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos
-              eligendi
-              labore.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-5">
-        <div class="card h-100">
-          <div class="card-body">
-            <img src="./img/mamp-satjuarez.jpg" class="rounded" width="99%" alt="">
-            <h4 class="card-title mt-4">SAT Ciudad de México</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at
-              dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos
-              eligendi
-              labore.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-5">
-        <div class="card h-100">
-          <div class="card-body">
-            <img src="./img/mamp-minaeleden.jpg" class="rounded" width="99%" alt="">
-            <h4 class="card-title mt-4">Mina el Edén</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at
-              dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos
-              eligendi
-              labore.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
 <?php
     include_once('footer.php');
 ?>
---
+++ modelos-materiales.php
@@ -0,0 +1,155 @@
+<?php
+    include_once('header.php');
+?>
+
+  <!-- Contenido Principal -->
+  <div class="container">
+    <div class="row align-items-center my-5">
+      <div class="col-lg-6">
+          <img class="img-fluid rounded mb-4 mb-lg-0" src="./img/m02.jpg" alt="Colores y Acabados de Mampras" /></div>
+      <div class="col-lg-6">
+        <h1 class="font-weight-bold py-2">Modelos y Materiales</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
+          makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
+          you want!</p>
+        <a class="btn btn-primary py-2" href="#!">Ver acabados</a>
+      </div>
+    </div>
+  </div>
+  <!-- Modelos -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-01.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 1</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-02.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 2</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-03.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 3</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-04.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 4</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-05.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 5</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-06.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 6</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-01.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 7</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <img src="./img/c-a-02.jpg" class="rounded" width="99%" alt="">
+            <h5 class="card-title mt-4 font-weight-bold">Modelo 8</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Opciones -->
+  <div class="container bg-light">
+    <div class="row ml-auto">
+      <h2 class="font-weight-bold mt-2">Opciones para cotizar</h2>
+    </div>
+    <div class="row align-items-center my-5">
+      <div class="col-lg-2">
+        <img src="./img/01-main.svg" alt="Opción 1">
+      </div>
+      <div class="col-lg-7">
+        <h4 class="font-weight-normal">Agenda una visita a obra</h4>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
+          makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
+          you want!</p>
+        <a class="btn btn-primary" href="#!">Agendar</a>
+      </div>
+      <div class="col-lg-3">
+        <img src="./img/person_main.svg" alt="Agenda una Cita">
+      </div>
+    </div>
+    <div class="row align-items-center my-5">
+      <div class="col-lg-2">
+        <img src="./img/02-main.svg" alt="Opción 2">
+      </div>
+      <div class="col-lg-7">
+        <h4 class="font-weight-normal">Sube tu plano</h4>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
+          makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
+          you want!</p>
+        <a class="btn btn-primary" href="#!">Subir</a>
+      </div>
+      <div class="col-lg-3">
+        <img src="./img/cloud_main.svg" alt="Sube tu plano">
+      </div>
+    </div>
+    <div class="row align-items-center my-5">
+      <div class="col-lg-2">
+        <img src="./img/03-main.svg" alt="Opción 3">
+      </div>
+      <div class="col-lg-7">
+        <h4 class="font-weight-normal">Cotiza en línea</h4>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it
+          makes a great use of the standard Bootstrap core components. Feel free to use this template for any project
+          you want!</p>
+        <a class="btn btn-primary" href="#!">Cotizar</a>
+      </div>
+      <div class="col-lg-3">
+        <img src="./img/sys_main.svg" alt="Cotiza tu proyecto el línea">
+      </div>
+    </div>
+  </div>
+
+  <?php
+    include_once('footer.php');
+?>
